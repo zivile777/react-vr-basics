af77c4cff61d97e15c5b3808b4b43e8b4ccd678d
zivile777 <zivile.goodwin@gmail.com>
2017-06-12 22:11:53 -0500

    lesson with shapes: cube, cylinder, sphere, plane

--- Basics/index.vr.js
+++ Basics/index.vr.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, Box, Cylinder, Sphere, Plane } from 'react-vr';
 
 export default class Basics extends Component {
-  constructor() {
-    super();
-    this.state = {
-      fontSize: 0.1,
-    };
-  }
-
   render () {
     return (
       <View>
         <Pano source={asset('zion-vr.jpg')}></Pano>
-        <Text
-          onEnter = {() => this.setState({ fontSize: 0.2 })}
-          onExit = {() => this.setState({ fontSize: 0.1 })}
-          style = {{
-            fontSize: this.state.fontSize,
-            transform: [{ translate: [0, 0, -2] },
-          ],
-          }}>
-          Hover over me!
-        </Text>
+        <Box
+          dimWidth={0.5}
+          dimHeight={0.5}
+          dimDepth={0.5}
+          wireframe={true}
+          style={{
+            color: '#dd2222',
+            transform: [
+              { translate: [0, 0, -3] },
+              { translateY: 1 },
+              { translateX: -0.5 },
+              { rotateY: 45 },
+              { rotateZ: 45 },
+            ],
+          }}
+        />
+        <Cylinder
+          dimHeight={0.5}
+          radiusBotton={0.5}
+          radiusTop={0.5}
+          segments={15}
+          wireframe={true}
+          style={{
+            color: '#2d2',
+            transform: [
+              { translate: [0, 0.5, -3] },
+              { rotateY: 45 },
+              { rotateZ: 45 },
+            ],
+          }}
+        />
+        <Sphere
+          radius={0.5}
+          widthSegments={10}
+          heightSegments={10}
+          texture={'http://i.imgur.com/bEBqA.jpg'}
+          style={{
+            color: '#aad',
+            transform: [
+              { translate: [1, 0, -3] },
+              { rotateY: 45 },
+            ],
+          }}
+        />
+        <Plane
+          dimHeight={2}
+          dimWidth={5}
+          texture={'http://i.imgur.com/3FAR9Kf.jpg'}
+          style={{
+            transform: [
+              { translate: [0, 0, -5] },
+              { rotateX: -65 },
+            ],
+          }}
+        />
       </View>
     );
   }
